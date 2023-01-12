291b9106838c09e3e99988b7e9c8e3481aa14c7e
laura romero <lauraromerocx@gmail.com>
2023-01-12 12:52:57 -0500

    cambios del main y data

--- src/data.js
+++ src/data.js
@@ -1,16 +1,45 @@
 import data from './data/ghibli/ghibli.js'
-//const dataMovies = data.films;
-//funciona pero toma todos los elementos del array, lo muestra como un objeto 
+//funcion que devuelve data a main
+const dataMovies = data.films;
+//funcion para ordenar años de lanzamiento asc. Toma los valores del elemento, los compara y devuelve la comparación
+export const sortMovies = (opUp, opDown) => {
+  //comparar elementos del objeto.
+  const compare = (a, b) => {
+//esta comparando release date, si a es mayor que b
+    if (a.release_date > b.release_date) {
+      return 1;
+    }
+    else {
+      return -1;
+    }
+  }
+  //sort option downward retorna 
+  if (opDown === "downward") {
+    //retorna opUp sort y toma la comparacion de arriba y reversa el array
+    return opUp.sort(compare).reverse();
+  }
+
+  else {
+    return opUp.sort(compare);
+  }
+
+};
+
+// como mejorar la funcion para cada uno de los directores
+
+export const Miyazaki = dataMovies.filter(function(film){
+  return film.director === 'Hayao Miyazaki'
+});
+
+export const Takahata = dataMovies.filter(function(film){
+  return film.director === 'Isao Takahata'
+});
+export const Kondo = dataMovies.filter(function(film){
+  return film.director === 'Yoshifumi Kondo'
+});
 /*
-export function randomMovie(dataMovies){
-  return dataMovies[Math.floor(Math.random()* dataMovies.length)]
-}
+Hiroyuki Morita
+Goro Miyazaki
+Hiromasa Yonebayashi
 */
-
-/* como mejorar la funcion para cada uno de los directores
-
-const Miyazaki = dataMovies.filter((dataMovies) => dataMovies.director === 'Hayao Miyazaki'
-); 
-console.log(Miyazaki)
-//funcionando, retorna solo peliculas de miyazaki pero el array  */
 
--- src/main.js
+++ src/main.js
@@ -1,38 +1,62 @@
 //import ghibli from './data/ghibli/ghibli.js'
 import data from './data/ghibli/ghibli.js'
-//import { randomMovie } from './data.js'
+import { sortMovies } from './data.js'
+//console.log(data)
+
 const dataMovies = data.films;
-//declaramos donde se mostraran las peliculas
 const moviesList = document.getElementById('showMovies');
+const sortSelectUp=document.getElementById('orderYear');
+
+
 //declaramos el html vacio para luego insertar con innerHtml
-let htmlCode = ``;
+//declaramos funcion para mostrar la data de las peliculas mediante un innerHTML
+function printMovies(){
+  let printHtml = ''; 
 //tomamos cada dato del array con un forEach, recorre todo el array y retorna los datos que elegimos más abajo
-dataMovies.forEach(function(singleMovieObjects)
-{
-  //imagen de arriety link roto
-  //elementos que se insertaran desde la data al html 
-  htmlCode = htmlCode +
+  dataMovies.forEach(function(singleMovie)
+  {
+  //elementos que se insertaran desde la data al html
+  //html += not an addition, but a full overwrite.  printHtml = printHtml+
+  printHtml = printHtml+
     `
-      <div>
       
-      <img src="${singleMovieObjects.poster}">
-      <p>Title: ${singleMovieObjects.title}</p>
-      <p>Description: ${singleMovieObjects.description}</p>
-      <p>Director: ${singleMovieObjects.director}</p>
-      <p>Producer: ${singleMovieObjects.producer}</p>
-      <p>Release date: ${singleMovieObjects.release_date}</p>
-
+      <div>
+      <img class="poster-Image" src="${singleMovie.poster}"/>
+      <p>Title: ${singleMovie.title}</p>
+      <p>Director: ${singleMovie.director}</p>
+      <p>Producer: ${singleMovie.producer}</p>
+      <p>Release date: ${singleMovie.release_date}</p>
       </div>
-
+      
   `;
-  /*console.log(htmlCode); funciona, luego tomamos movieList definido más arriba para insertarlo
-//we now say let the inside of that div take in our htmlCode variable that holds our html codes.*/
-});
-moviesList.innerHTML= htmlCode
+    //we now say let the inside of that div take in our htmlCode variable that holds our html codes.*/
+  });
+  //se insertan los datos en el container de HTML
+  moviesList.innerHTML= printHtml;
+}
+printMovies(dataMovies);
 
+//se ejecuta la funcion con el change en el orderYear, evento desde el DOM
+sortSelectUp.addEventListener('click', MoviesSortUp)
+function MoviesSortUp(){
+  let sortRelease = sortSelectUp.value;
+  let moviesSorted;
+  //moviesSorted va a ser la funcion de sort, tomando la data y el value del id
+  moviesSorted=sortMovies(dataMovies, sortRelease);
+  printMovies(MoviesSortUp);
+} 
+
+//  for each o llamado individual
+//printMovies(nombreFuncion)
+
+/*Miyazaki.forEach(function(film){
+console.log(film.title +' '+ film.director + ' ' + film.release_date)});
+Takahata.forEach(function(film){
+  console.log(film.title +' '+ film.director + ' ' + film.release_date)});
 /*
-//specie funcion filter para HU penelope sub categoria
-//director funcion filter HU Omar
-//title para sort? HU camila
+specie funcion filter para HU penelope sub categoria
+director funcion filter HU Omar
 release_date sort? HU camila
+orden aleatorio zoe
 */
+// funcio orden alfabetico 
