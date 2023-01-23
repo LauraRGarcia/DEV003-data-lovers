5d71c23530b5c6e96a4c9b8bc56ec1cf24bbe2a4
laura romero <lauraromerocx@gmail.com>
2023-01-23 09:27:28 -0500

    funciones de la Z-A en data y main

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export const sortMovies = (opUp, opDown) => {
       return -1;
     }
   }
-  //sort option downward retorna 
+  //sort option downward retorna
   if (opDown === "downward") {
     //retorna opUp sort y toma la comparacion de arriba y reversa el array
     return opUp.sort(compare).reverse();
@@ -25,21 +25,47 @@ export const sortMovies = (opUp, opDown) => {
 
 };
 
-// como mejorar la funcion para cada uno de los directores
+// declaracion de funcion ordenar de la A-Z y Z-A
+//export function sortName (dataMovies, sortFinish)
+/*const sortAz = dataMovies.array.sort ((a,b)=>{
+  if(a.name.toLowerCase() < b.name.toLowerCase()) {
+return 1;
+  }
+if(a.name.toLowerCase() > b.name.toLowerCase()) {
+return -1;
+}
+return 0;
+});
+const sortZa = dataMovies.sort ((a,b)=>{
+  if(a.name.toLowerCase() > b.name.toLowerCase()) {
+return -1;
+  }
+if(a.name.toLowerCase() < b.name.toLowerCase()) {
+return 1;
+}
+return 0;
+});
+if(sortFinish === A-Z){
+return sortAz;
+} else if(sortFinish === Z-A){
+  return sortZa;
+};*/
+// filtro de director
+//*export const filterDirector =(dataMovies, conditionDirector) =>{
+  //Filter es el toma el array del "dataMovies//
+/*const filterDir = dataMovies.filter((dataMovies) =>{
+if (conditionDirector === dataMovies.director){
+return true;
+}
+else{
+return false;
+}
+});
+return filterDir;
+};
 
-export const Miyazaki = dataMovies.filter(function(film){
-  return film.director === 'Hayao Miyazaki'
-});
 
-export const Takahata = dataMovies.filter(function(film){
-  return film.director === 'Isao Takahata'
-});
-export const Kondo = dataMovies.filter(function(film){
-  return film.director === 'Yoshifumi Kondo'
-});
-/*
-Hiroyuki Morita
-Goro Miyazaki
-Hiromasa Yonebayashi
-*/
+
+
+
 
--- src/main.js
+++ src/main.js
@@ -1,25 +1,27 @@
 //import ghibli from './data/ghibli/ghibli.js'
 import data from './data/ghibli/ghibli.js'
-import { sortMovies } from './data.js'
+import { sortMovies, sortName, filterDirector } from './data.js'
 //console.log(data)
 
 const dataMovies = data.films;
 const moviesList = document.getElementById('showMovies');
-const sortSelectUp=document.getElementById('orderYear');
+const sortSelectUp = document.getElementById('orderYear');
+const sortSelectName =document.getElementById('sortAbc');
+const filterMovies = document.getElementById('orderDirector');
 
 
 //declaramos el html vacio para luego insertar con innerHtml
 //declaramos funcion para mostrar la data de las peliculas mediante un innerHTML
 function printMovies(){
-  let printHtml = ''; 
+  let printHtml = '';
 //tomamos cada dato del array con un forEach, recorre todo el array y retorna los datos que elegimos más abajo
   dataMovies.forEach(function(singleMovie)
   {
   //elementos que se insertaran desde la data al html
   //html += not an addition, but a full overwrite.  printHtml = printHtml+
-  printHtml = printHtml+
+  printHtml +=
     `
-      
+
       <div>
       <img class="poster-Image" src="${singleMovie.poster}"/>
       <p>Title: ${singleMovie.title}</p>
@@ -27,7 +29,7 @@ function printMovies(){
       <p>Producer: ${singleMovie.producer}</p>
       <p>Release date: ${singleMovie.release_date}</p>
       </div>
-      
+
   `;
     //we now say let the inside of that div take in our htmlCode variable that holds our html codes.*/
   });
@@ -44,38 +46,22 @@ function MoviesSortUp(){
   //moviesSorted va a ser la funcion de sort, tomando la data y el value del id
   moviesSorted=sortMovies(dataMovies, sortRelease);
   printMovies(MoviesSortUp);
-} 
+};
+// función orden A-Z y Z-A 
+/*sortSelectName.addEventListener('click',movieSortName )
+function movieSortName () {
+  let movieByName = sortSelectName.value;
+  let nameSortSelect;
+  //namesortselect toma la funcion que esta tomando sortName (que es la funcion del data) y el moviebyName (los value del html)
+nameSortSelect= sortName (dataMovies, movieByName);
+printMovies(movieSortName);
+}*/
+//funcion orden de director //
+/*filterMovies.addEventListener('click',selectFilterMovie )
+function selectFilterMovie() {
+  let nameDirector = filterMovies.value;
+  let optionDirector;
+  optionDirector= filterDirector (dataMovies, nameDirector);
+  printMovies(selectFilterMovie);
+}
 
-//  for each o llamado individual
-//printMovies(nombreFuncion)
-
-/*Miyazaki.forEach(function(film){
-console.log(film.title +' '+ film.director + ' ' + film.release_date)});
-
-Takahata.forEach(function(film){
-  console.log(film.title +' '+ film.director + ' ' + film.release_date)});
-
-//se ejecuta la funcion con el change en el orderYear, evento desde el DOM
-sortSelectUp.addEventListener('click', MoviesSortUp)
-function MoviesSortUp(){
-  let sortRelease = sortSelectUp.value;
-  let moviesSorted;
-  //moviesSorted va a ser la funcion de sort, tomando la data y el value del id
-  moviesSorted=sortMovies(dataMovies, sortRelease);
-  printMovies(MoviesSortUp);
-} 
-
-//  for each o llamado individual
-//printMovies(nombreFuncion)
-
-/*Miyazaki.forEach(function(film){
-console.log(film.title +' '+ film.director + ' ' + film.release_date)});
-Takahata.forEach(function(film){
-  console.log(film.title +' '+ film.director + ' ' + film.release_date)});
-/*
-specie funcion filter para HU penelope sub categoria
-director funcion filter HU Omar
-release_date sort? HU camila
-orden aleatorio zoe
-*/
-// funcio orden alfabetico 
