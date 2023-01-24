5438fcc9543235b87ef0df45d208de10facf3a51
laura romero <lauraromerocx@gmail.com>
2023-01-24 16:32:35 -0500

    agregue export al data linea 3

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 import data from './data/ghibli/ghibli.js'
 //funcion que devuelve data a main
-const dataMovies = data.films;
+export const dataMovies = data.films;
 
 //funcion para ordenar años de lanzamiento asc. Toma los valores del elemento, los compar y devuelve la comparación
 export const sortMovie = (opUp, opDown) => {
